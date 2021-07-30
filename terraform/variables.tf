adead2edbc6116722e9acc1ec7e29bd8f81b4231
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2021-07-29 21:23:44 -0700

    change config-local.js

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -36,10 +36,16 @@ locals {
     define([], function () {
       var configLocal = {};
 
+      // clearing local storage otherwise source cache will obscure the override settings
+      localStorage.clear();
+
+      var getUrl = window.location;
+      var baseUrl = getUrl.protocol + "//" + getUrl.host;
+
       // WebAPI
       configLocal.api = {
         name: 'OHDSI',
-        url: 'https://${var.prefix}-${var.environment}-omop-appservice.azurewebsites.net:8080/WebAPI/'
+        url: baseUrl + '/WebAPI/'
       };
 
       configLocal.cohortComparisonResultsEnabled = false;

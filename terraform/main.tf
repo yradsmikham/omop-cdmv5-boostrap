638b948ab112812a14f3c2fc0feb17d60958799b
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2021-08-12 15:54:53 -0700

    remove depends on for sql db

--- terraform/main.tf
+++ terraform/main.tf
@@ -156,9 +156,9 @@ resource "azurerm_mssql_database" "OHDSI-CDMV5" {
   #}
 
   # convert vocabulary table columns from varchar to date
-  provisioner "local-exec" {
-      command = "sqlcmd -U omop_admin -P ${var.omop_password} -S ${var.prefix}-${var.environment}-omop-sql-server.database.windows.net -d ${var.prefix}_${var.environment}_omop_db -i '../sql/convert_varchar_to_date.sql' -o ${var.log_file}"
-  }
+  #provisioner "local-exec" {
+  #    command = "sqlcmd -U omop_admin -P ${var.omop_password} -S ${var.prefix}-${var.environment}-omop-sql-server.database.windows.net -d ${var.prefix}_${var.environment}_omop_db -i '../sql/convert_varchar_to_date.sql' -o ${var.log_file}"
+  #}
 
   # import synpuf data
   #provisioner "local-exec" {
@@ -166,17 +166,14 @@ resource "azurerm_mssql_database" "OHDSI-CDMV5" {
   #}
 
   # add indices and primary keys
-  provisioner "local-exec" {
-      command = "sqlcmd -U omop_admin -P ${var.omop_password} -S ${var.prefix}-${var.environment}-omop-sql-server.database.windows.net -d ${var.prefix}_${var.environment}_omop_db -i .'./sql/OMOP_CDM_sql_server_indexes.sql' -o ${var.log_file}"
-  }
+  #provisioner "local-exec" {
+  #    command = "sqlcmd -U omop_admin -P ${var.omop_password} -S ${var.prefix}-${var.environment}-omop-sql-server.database.windows.net -d ${var.prefix}_${var.environment}_omop_db -i .'./sql/OMOP_CDM_sql_server_indexes.sql' -o ${var.log_file}"
+  #}
 
   # add foreign key constraints
-  provisioner "local-exec" {
-      command = "sqlcmd -U omop_admin -P ${var.omop_password} -S ${var.prefix}-${var.environment}-omop-sql-server.database.windows.net -d ${var.prefix}_${var.environment}_omop_db -i '../sql/OMOP_CDM_sql_server_constraints.sql' -o ${var.log_file}"
-  }
-  depends_on = [
-    azurerm_mssql_server.omop_sql_server
-  ]
+  #provisioner "local-exec" {
+  #    command = "sqlcmd -U omop_admin -P ${var.omop_password} -S ${var.prefix}-${var.environment}-omop-sql-server.database.windows.net -d ${var.prefix}_${var.environment}_omop_db -i '../sql/OMOP_CDM_sql_server_constraints.sql' -o ${var.log_file}"
+  #}
 }
 
 # This creates the plan that the service use
